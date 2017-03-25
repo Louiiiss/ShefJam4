8180940dd95d90d191c465b7a4a69a8aef312fc1
sam Dickinson <sdickinson1@sheffield.ac.uk>
2017-03-25 19:32:22 +0000

    Working Grid Highlight System

---
+++ Assets/Scripts/UIItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIItem : MonoBehaviour, IDragHandler, IDropHandler {
+
+	public int XSlots;
+	public int YSlots;
+	GameObject Grid;
+
+	// Use this for initialization
+	void Start () {
+		Grid = GameObject.Find ("Grid");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnDrag(PointerEventData data){
+		Grid.GetComponent<Grid> ().CurrentObjSizeX = XSlots;
+		Grid.GetComponent<Grid> ().CurrentObjSizeY = YSlots;
+	}
+
+	public void OnDrop(PointerEventData data){
+		print ("Dropped");
+	}
+
+}
